97984a8acfeaecba1bf9bb8a97090757ea555150
Evan Hallmark <ehallmar@usc.edu>
2021-11-11 14:46:25 -0800

    updates

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,20 +1,37 @@
 <script>
+import { navigateLink, page } from '../js/navigation-utils'
 
 </script>
 
 <style>
     .buttons{
         margin: 2rem auto 1rem;
+        max-width: 550px;
     }
 </style>
 
-<div align="middle" class="buttons">
+<div align="center" class="buttons">
     <h2>
         About Gamma Phi
     </h2>
     
     <p>
-        Gamma Phi is a suite of fair gambling games on Lamden.
+        Gamma Phi is a suite of fair gambling games built on the Lamden blockchain.
     </p>
+
+    <h4>
+        What does "fair" mean?
+    </h4>
+
+    <p>
+        Fair means that the expected return for any of our games is exactly 0%.
+        All of our games are commission-free, and we never take a percentage of your winnings. 
+        So is this even gambling? Yes. It's the future of gambling.
+    </p>
+
+
 </div>
 
+<div align="center" class="buttons">
+    <a class="about-link" on:click={navigateLink} href="/roadmap">See Roadmap</a>
+</div>
